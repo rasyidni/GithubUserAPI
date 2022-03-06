ac2627b276b007fbfb0d1c8006516b457e70e11f
rasyidni <rasyidni700@gmail.com>
2022-03-06 17:37:38 +0700

    commit project pertama

--- app/src/main/java/com/example/githubuserapi/UI/Main/MainActivity.kt
+++ app/src/main/java/com/example/githubuserapi/UI/Main/MainActivity.kt
@@ -1,19 +1,37 @@
 package com.example.githubuserapi.UI.Main
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.githubuserapi.R
 import com.example.githubuserapi.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var edtWidth: EditText
+    private lateinit var edtHeight: EditText
+    private lateinit var edtLength: EditText
+    private lateinit var btnCalculate: Button
+    private lateinit var tvResult: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
+
+        edtWidth = findViewById(R.id.edt_width)
+        edtHeight = findViewById(R.id.edt_height)
+        edtLength = findViewById(R.id.edt_length)
+        btnCalculate = findViewById(R.id.btn_calculate)
+        tvResult = findViewById(R.id.tv_result)
+
+        btnCalculate.setOnClickListener(this)
     }
 
-    //tes aja
-    //Apakah muncul
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
+    }
+
 }
